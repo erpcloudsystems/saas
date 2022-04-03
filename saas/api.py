0592bf5b2723e5424c64c736737bfa933c9c2b70
kamalghanima <kamalghanima123@gmail.com>
2022-04-03 16:38:02 +0300

    delete server doctype ,add buttons js and create new api called notify client

--- saas/api.py
+++ saas/api.py
@@ -13,50 +13,15 @@ def delete_account(doc, method):
     site = frappe.get_doc("Site", doc.name)
     enqueue(delete_site, site=site)
 
-def notify_user(doc, method):
-    site = frappe.get_doc("Site", doc.name)
-    site.check_permission("email")
-
-    if site.status == "Pending Approval":
-        site.email_verification_code = id_generator()
-        # frappe.sendmail(
-        #     recipients = [site.email],
-        #     sender='arwema@gmail.com',
-        #     subject="Validate your account",
-        #     message = "Please validate your email, click on this link: http://saas.erp:8000/api/method/saas.api.verify_account?name=%s&code=%s" % (site.name,site.email_verification_code),
-        #     reference_doctype=site.doctype,
-        #     reference_name=site.name
-        # )
-        site.status = "Email Sent"
-        site.save()
-        frappe.msgprint(_("Confirmation emails sent"))
-    else:
-        frappe.msgprint(_("Site Status must be 'Pending Approval'"))
-
-
-@frappe.whitelist(allow_guest=True)
-def verify_account(name, code):
-    site = frappe.get_doc("Site", name)
-    if site.status != "Email Sent":
-        return "The site does not need verification"
-    if site.email_verification_code == code:
-        site.status = "Site Verified"
-        site.flags.ignore_permissions = True
-        site.save()
-        enqueue(create_site, site=site.name)
-        return "Site Verified"
-    else:
-        return "Wapi"
-
 @frappe.whitelist()
 def create_site(site):
     site = frappe.get_doc("Site", site)
-    cmd = ["bench", "new-site", "--db-name", site.name, "--mariadb-root-username", "root", "--mariadb-root-password", 'password', "--admin-password", "logic", "--install-app", "erpnext", site.title]
+    cmd = ["bench", "new-site", "--db-name", site.name, "--mariadb-root-username", "root", "--mariadb-root-password", '123', "--admin-password", "logic", "--install-app", "erpnext", site.title]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                                         stderr=subprocess.PIPE,
                                         stdin=subprocess.PIPE,
                                         # cwd="/home/frappe/frappe-bench"
-                                        cwd="/Volumes/AhmadSSD/frappeworkspace/anvilerp2")
+                                        cwd="/home/kamal/work/v13/saas")
     out,err = p.communicate()
     if not err:
         """Create an orientation meeting when a new User is added"""
@@ -69,36 +34,30 @@ def create_site(site):
         # the System Manager might not have permission to create a Meeting
         lead.flags.ignore_permissions = True
         lead.insert()
-        # frappe.sendmail(
-        #     recipients = [site.email],
-        #     sender="arwema@gmail.com",
-        #     subject="Your ERP Account",
-        #     message = "Dear Customer, your account has been create and accessible from: http://%s:8000" % (site.title),
-        #     reference_doctype=site.doctype,
-        #     reference_name=site.name
-        # )
-    
 
-def delete_site(site):
-    cmd = ["bench", "drop-site","--root-password", 'frappe', site.title]
+@frappe.whitelist()
+def notify_client(site):
+    site = frappe.get_doc("Site", site)
+    frappe.sendmail(
+    recipients = [site.email],
+    sender="arwema@gmail.com",
+    subject="Your ERP Account",
+    message = "Dear Customer, your account has been create and accessible from: http://%s:8000" % (site.title),
+    reference_doctype=site.doctype,
+    reference_name=site.name
+    )
+
+@frappe.whitelist()
+def delete_site(name):
+    site = frappe.get_doc("Site", name)
+    cmd = ["bench", "drop-site","--root-password", '123', site.title]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                                         stderr=subprocess.PIPE,
                                         stdin=subprocess.PIPE,
-                                        cwd="/home/frappe/frappe-bench")
+                                        cwd="/home/kamal/work/v13/saas")
     out,err = p.communicate()
     if not err:
-        frappe.sendmail(
-            recipients = [site.email],
-            sender="arwema@gmail.com",
-            subject="Your ERP Account Terminated",
-            message = "Dear Customer, your account has been terminated",
-            reference_doctype=site.doctype,
-            reference_name=site.name
-        )
         lead = frappe.get_doc("Lead", site.title)
         lead.flags.ignore_permissions = True
         lead.status = "Do Not Contact"
         lead.save()
-    
-
-
