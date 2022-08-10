cfdc5dd47d9872fb7ffd8f625f89487f2d4b3c53
zaqoutabed <zaqoutabed@gmail.com>
2022-08-10 16:21:00 +0300

    delete unused files

--- saas/io_connector.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-def _socketio_connector(doc, method):
-    users = 12
-    companies = 2
-    
-    frappe.publish_realtime("_socketio_connector", "{'message':'test','custom_app'}")
